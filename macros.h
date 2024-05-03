c572d2fdd00119e529f01104943453fa36c4245f
leviongit <levi.duncan.self@gmail.com>
2024-05-04 00:24:50 +0200

    clean everything; start anew

--- macros.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDED_MACROS_H
-#define INCLUDED_MACROS_H
-
-#define alloc_zero(type, name)                                                 \
-  type *name = malloc(sizeof(type));                                           \
-  *name = (type) {}
-
-#endif
